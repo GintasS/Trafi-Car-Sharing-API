fc66e4c733650eb81549c0b21f62a78acc1f009a
GintasS <svedgintas@gmail.com>
2021-09-14 17:34:13 +0300

    Added Helper files.

---
+++ src/TrafiCarSharingAPI/TrafiCarSharingAPI/Helpers/Database.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TrafiCarSharingAPI.Core.Entities;
+
+namespace TrafiCarSharingAPI.Core.Helpers
+{
+    public static class Database
+    {
+        public static List<Car> Cars { get; set; } = new List<Car>();
+        public static List<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}
