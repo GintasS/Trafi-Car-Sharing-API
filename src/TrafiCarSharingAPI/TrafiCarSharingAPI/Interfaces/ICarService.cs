ae4c1b44159857bf006db6d8c2cb96d8af09a3af
GintasS <svedgintas@gmail.com>
2021-09-14 17:32:24 +0300

    Added Interfaces.

---
+++ src/TrafiCarSharingAPI/TrafiCarSharingAPI/Interfaces/ICarService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TrafiCarSharingAPI.Core.Entities;
+
+namespace TrafiCarSharingAPI.Core.Interfaces
+{
+    public interface ICarService
+    {
+        public Task<IEnumerable<Car>> GetAllCars();
+        public Task<IEnumerable<Car>> GetAllCarsWithDeviceState();
+        public Task<DeviceState> GetSingleCarDeviceState();
+        public Task<Guid> StartCarBooking(Car car);
+        public Task<Guid> FinishCarBooking(Booking booking);
+        public Task<Car> FindCarById(string carId);
+        public Task<Booking> FindBookingByCarId(string carId);
+        public Task<Booking> FindBookingByBookingId(string bookingId);
+        public Task<Car> FindCarByBookingId(string bookingId);
+        public Task<bool> UnlockCar(DeviceState device);
+    }
+}
---
+++ src/TrafiCarSharingAPI/TrafiCarSharingAPI/Interfaces/IQueueService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Amazon.SQS.Model;
+
+namespace TrafiCarSharingAPI.Core.Interfaces
+{
+    public interface IQueueService
+    {
+        Task<ReceiveMessageResponse> GetMessage(string qUrl, int waitTime = 0);
+        Task DeleteMessage(Message message, string qUrl);
+        bool ProcessMessage(Message message);
+        Task SendMessage(SendMessageRequest messageRequest);
+    }
+}
