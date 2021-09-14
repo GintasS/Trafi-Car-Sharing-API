0f60fd5fe6dfce77605d8d2386b17348de23eace
GintasS <svedgintas@gmail.com>
2021-09-14 17:35:12 +0300

    Added Constants.

---
+++ src/TrafiCarSharingAPI/TrafiCarSharingAPI/Configuration/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace TrafiCarSharingAPI.Core.Configuration
+{
+    public static class Constants
+    {
+        public static string DeviceStateQueue = "https://sqs.eu-west-1.amazonaws.com/577284730396/go-sharing-ruptela-states.fifo";
+        public static string DeviceCommandQueue = "https://sqs.eu-west-1.amazonaws.com/577284730396/go-sharing-ruptela-commands.fifo";
+
+        public static int WaitTime = 2;
+    }
+}
